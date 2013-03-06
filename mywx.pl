88cdd3739901fc815280efdb9c741fd4a757a570
Tim Gimmel <tim@gimmel.org>
2013-03-05 21:45:38 -0600

    Fixed heat index problem.  I hope!   --Tim

--- mywx.pl
+++ mywx.pl
@@ -31,8 +31,8 @@ if ($Report->{error})
   {
     print "$Report->{errortext}";
   }
-$winddirtext = $Report->{winddirtext};
-if ($winddirtext eq "Calm") { $nochill = "1"; }
+#$winddirtext = $Report->{winddirtext};
+#if ($winddirtext eq "Calm") { $nochill = "1"; }
 print "\n";
 if ($debug) { print "debug: $Report->{obs}\n"; }
 print "================================================================\n";
@@ -41,11 +41,11 @@ print "================================================================\n";
 printf "Temperture:%3u degrees             Air Pressure:%2.2f Inches hg\n", $Report->{temperature_f}, $Report->{pressure_inhg};
 printf " Windspeed:%3u mph from %-9s  Guest %s mph \n", $Report->{windspeedmph}, $Report->{winddirtext}, $Report->{windgustmph};
 printf "  Dewpoint:%3u degrees             Relative humidity:%u%% \n", $Report->{dewpoint_f}, $Report->{relative_humidity};
-if ($Report->{windchill_f}) { printf " Windchill:%3u degrees", $Report->{windchill_f} } else { print "  No Windchill Report   "; }
-if ($Report->{heat_index_f}) { printf "             Heat Index:%3u \n", $Report->{heat_index_f}; }
+if ($Report->{windchill_f}) { printf " Windchill:%3u degrees", $Report->{windchill_f} } else { print "  No Windchill Report "; }
+if ($Report->{heat_index_f} and ($Report->{temperature_f} > 60)) {
+    printf "             Heat Index:%3u \n", $Report->{heat_index_f};
+    } else { print "             No Heat Index Report\n"; }
 if ($Report->{cloudcover}) {printf "Cloudcover: %s \n", $Report->{cloudcover}; }
-
-#print "Conditions: $Report->{conditionstext}\n";
 print "================================================================\n";
 print "Find your code at http://www.aircharterguide.com/Airports \n";
 print "================================================================\n";
