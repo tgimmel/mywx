5810174bcbcf6fc2ca2b4c160097ec51e9f84717
Tim Gimmel <tim@thinky.(none)>
2013-03-16 22:51:10 -0500

    Gust was misspelled as "Guest", fixed.
    --Tim

--- mywx.pl
+++ mywx.pl
@@ -71,7 +71,7 @@ print "            Report for $metr{ uc($station) }\n";
 print "  WX report from station $Report->{code} \@$Report->{report_date} $Report->{report_time} UTC\n";
 print "================================================================\n";
 printf "Temperture:%3u degrees             Air Pressure:%2.2f Inches hg\n", $Report->{temperature_f}, $Report->{pressure_inhg};
-printf " Windspeed:%3u mph from %-9s  Guest %s mph \n", $Report->{windspeedmph}, $Report->{winddirtext}, $Report->{windgustmph};
+printf " Windspeed:%3u mph from %-9s  Gust %s mph \n", $Report->{windspeedmph}, $Report->{winddirtext}, $Report->{windgustmph};
 printf "  Dewpoint:%3u degrees             Relative humidity:%u%% \n", $Report->{dewpoint_f}, $Report->{relative_humidity};
 if ($Report->{windchill_f}) { printf " Windchill:%3u degrees", $Report->{windchill_f} } else { print "  No Windchill Report "; }
 if ($Report->{heat_index_f} and ($Report->{temperature_f} > 60)) {
